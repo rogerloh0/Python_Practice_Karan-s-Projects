3a56dc3c3cee18cc0507fc2d1240c91074798da8
rogerloh0 <rogerloh4.0@gmail.com>
2017-03-31 02:18:32 +0800

    Update Practice01-1-Find_PI_to_the_Nth_Digit.py

--- Practice01-1-Find_PI_to_the_Nth_Digit.py
+++ Practice01-1-Find_PI_to_the_Nth_Digit.py
@@ -1,6 +1,4 @@
 import math
 X = input("Please enter how many decimal points you wish to allow your PI: ")
-#print(type(int(X)))
 print(round(math.pi,int(X)))
-#newPi = round(math.pi, X)
-#print(type(newPi))
+
